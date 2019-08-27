33cf83d781e63f13203c33f48dfb6af4c669060e
Sebas <seplarui@gmail.com>
2019-08-27 23:05:40 +0200

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
     console.log("Servidor ON - Puerto: 3000");
