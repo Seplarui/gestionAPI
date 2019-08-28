03f789e21c2c00b1120f977f79b219b69d507338
Sebas <seplarui@gmail.com>
2019-08-28 23:21:17 +0200

    Correction testSpec.js

--- spec/testSpec.js
+++ spec/testSpec.js
@@ -2,10 +2,10 @@ const request= require('request');
 const server = require('../index');
 
 
-const endpoint = 'htpt://localhost:3000/hola';
+const endpoint = 'http://localhost:3000/hola';
 
 describe('hola',function() {
-    it('should return 200 response code', function () {
+    it('should return 200 response code', function (done) {
         request.get(endpoint,function(error, response){
             if (!error && response.statusCode == 200) {
                 done();
