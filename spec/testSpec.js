00253d15b925681d09baa2b9afa66c5c26d4340c
Sebas <seplarui@gmail.com>
2019-08-28 23:53:32 +0200

    Check API ON-Hola Mundo

--- spec/testSpec.js
+++ spec/testSpec.js
@@ -4,7 +4,7 @@ const server = require('../index');
 
 const endpoint = 'http://localhost:3000/hola';
 
-describe('hola',function() {
+describe('API ON',function() {
     it('should return 200 response code', function (done) {
         request.get(endpoint,function(error, response){
             if (!error && response.statusCode == 200) {
@@ -12,4 +12,20 @@ describe('hola',function() {
             }
         })
     });
-})
+    it('Check Hola Mundo', function(done) {
+        request.get(endpoint, function(error, response, body) {
+            expect(body).toBe("Hola Mundo");
+            done();
+        });
+    });
+});
+
+/*describe ('Hola Mundo', function() {
+    it('the correct word', function(done) {
+        const defaultButtonText = "Hola Mundo";
+       // body = document.getElementsByTagName
+       //expect(view.el.html().indexOf('Hola Mundo') != -1 ).toBe(true);
+       //expect(reviewElement.getText()).toEqual("My text");
+       expect(document.getElementsByTagName("body")).toEqual("kk");
+    })
+})*/
