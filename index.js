2797225f61078c1628d27bfb520df4442ba8e62b
Sebas <seplarui@gmail.com>
2019-08-29 18:30:12 +0200

    Delelte changes

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
 app.listen(3000, () => {
     console.log("Servidor ON - Puerto: 3000");
 })
