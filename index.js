564641fe3179bb2c82b34bc32107b0aefb658023
Sebas <seplarui@gmail.com>
2019-08-29 14:07:19 +0200

    Add CORS/Return JSON

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 app.listen(3000, () => {
     console.log("Servidor ON - Puerto: 3000");
 })
 
 app.get('/', function(req,res) {
-    res.send("Hola Mundo");
+    res.json("Hola Mundo");
 })
