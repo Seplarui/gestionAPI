d1d6b72fa0ef93978e36d53642a6d7c37c28ea38
Sebas <seplarui@gmail.com>
2019-08-30 13:29:34 +0200

    Add CORS

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.listen(3000, () => {
     console.log("Servidor ON - Puerto: 3000");
 })
 
-app.get('', function(req,res) {
-    res.send("Hola Mundo");
+//CORS
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    next();
+})
+
+app.get('/hola', function(req,res) {
+    res.send('Hola Mundo');
 })
